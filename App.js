8f9d418e6a011bb6be1c27b4741a21823986144e
daiju81 <1d07c.m8k.prv@gmail.com>
2020-05-31 01:48:54 +0900

    list add

--- App.js
+++ App.js
@@ -6,19 +6,34 @@ import {
   StatusBar,
   Platform,
   ScrollView,
+  FlatList,
 } from 'react-native';
 
 const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : StatusBar.currentHeight;
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: [
+        {index: 1, title: '原稿を書く', done: false},
+        {index: 2, title: '犬の散歩をする', done: false},
+      ],
+      currentIndex: 2,
+    };
+  }
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.filter}>
           <Text>Filterがここに配置されます</Text>
         </View>
-        <ScrollView>
-          <Text>Todoリスト</Text>
+        <ScrollView style={styles.todolist}>
+          <FlatList
+            data={this.state.todo}
+            renderItem={({item}) => <Text>{item.title}</Text>}
+            keyExtractor={(item, index) => 'todo_' + item.index}
+          />
         </ScrollView>
         <View style={styles.input}>
           <Text>テキスト入力がここに配置されます</Text>
